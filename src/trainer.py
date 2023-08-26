57fd89d1b8e5bc2cec752f6dec5517d0a16af4d5
Yunzhong Hou <hou_yz@outlook.com>
2023-08-26 19:12:02 +1000

    logging

--- src/trainer.py
+++ src/trainer.py
@@ -269,6 +269,8 @@ class PerspectiveTrainer(object):
         self.writer.add_scalar("losses/value_loss", v_loss.item(), self.rl_global_step)
         self.writer.add_scalar("losses/policy_loss", pg_loss.item(), self.rl_global_step)
         self.writer.add_scalar("losses/entropy", entropy_loss.item(), self.rl_global_step)
+        self.writer.add_scalar("charts/logstd", self.model.control_module.actor_logstd.mean().item(),
+                               self.rl_global_step)
         self.writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), self.rl_global_step)
         self.writer.add_scalar("losses/approx_kl", approx_kl.item(), self.rl_global_step)
         self.writer.add_scalar("losses/clipfrac", np.mean(clipfracs), self.rl_global_step)
