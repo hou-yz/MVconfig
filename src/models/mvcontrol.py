072c606fadfb94b9b1ff4afba34f0f1abf86cd32
Yunzhong Hou <hou_yz@outlook.com>
2023-08-26 19:13:10 +1000

    Update mvcontrol.py

--- src/models/mvcontrol.py
+++ src/models/mvcontrol.py
@@ -26,35 +26,28 @@ class CamControl(nn.Module):
     def __init__(self, dataset, hidden_dim, arch='transformer', actstd_init=1.0):
         super().__init__()
         self.arch = arch
+        self.feat_branch = nn.Sequential(nn.Conv2d(hidden_dim, hidden_dim, 3, 2, 1),
+                                         nn.ReLU(), nn.MaxPool2d(2, 2),
+                                         nn.Conv2d(hidden_dim, hidden_dim, 3, 2, 1),
+                                         nn.ReLU(), nn.MaxPool2d(2, 2),
+                                         nn.Conv2d(hidden_dim, hidden_dim, 3, 2, 1),
+                                         nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten())
+        self.config_branch = nn.Sequential(
+            nn.Linear(dataset.config_dim * (1 if arch == 'transformer' else dataset.num_cam), hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim))
         if arch == 'transformer':
             # transformer
-            self.feat_branch = nn.Sequential(nn.Conv2d(hidden_dim, hidden_dim, 3, 2, 1),
-                                             nn.LeakyReLU(),
-                                             nn.Conv2d(hidden_dim, hidden_dim, 3, 2, 1),
-                                             nn.LeakyReLU(),
-                                             nn.AdaptiveAvgPool2d((1, 1)))
-            self.config_branch = nn.Sequential(nn.Linear(dataset.config_dim, hidden_dim), nn.LeakyReLU(),
-                                               nn.Linear(hidden_dim, hidden_dim), nn.LeakyReLU(), )
             self.positional_embedding = create_pos_embedding(dataset.num_cam, hidden_dim)
             # CHECK: batch_first=True for transformer
             self.transformer = nn.TransformerEncoderLayer(hidden_dim, 8, hidden_dim * 4, batch_first=True)
             self.state_token = nn.Parameter(torch.randn(hidden_dim))
-        else:
-            # conv + fc only
-            self.feat_branch = nn.Sequential(nn.Conv2d(hidden_dim, hidden_dim, 3, 2, 1),
-                                             nn.LeakyReLU(),
-                                             nn.Conv2d(hidden_dim, hidden_dim, 3, 2, 1),
-                                             nn.LeakyReLU(),
-                                             nn.AdaptiveAvgPool2d((1, 1)))
-            self.config_branch = nn.Sequential(nn.Linear(dataset.config_dim * dataset.num_cam, hidden_dim),
-                                               nn.LeakyReLU(),
-                                               nn.Linear(hidden_dim, hidden_dim), nn.LeakyReLU(), )
 
-        self.critic = nn.Sequential(layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.LeakyReLU(),
-                                    layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.LeakyReLU(),
+        self.critic = nn.Sequential(layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.ReLU(),
+                                    layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.ReLU(),
                                     layer_init(nn.Linear(hidden_dim, 1), std=1.0))
-        self.actor_mean = nn.Sequential(layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.LeakyReLU(),
-                                        layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.LeakyReLU(),
+        self.actor_mean = nn.Sequential(layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.ReLU(),
+                                        layer_init(nn.Linear(hidden_dim, hidden_dim)), nn.ReLU(),
                                         layer_init(nn.Linear(hidden_dim, dataset.action_dim), std=0.01))
         self.actor_logstd = nn.Parameter(torch.ones(1, dataset.action_dim) * np.log(actstd_init))
 
@@ -67,26 +60,20 @@ class CamControl(nn.Module):
 
         if self.arch == 'transformer':
             # transformer
-            x_feat = self.feat_branch(feat.flatten(0, 1))[:, :, 0, 0].unflatten(0, [B, N])
-            x_config = self.config_branch(configs.to(feat.device).flatten(0, 1)).unflatten(0, [B, N])
+            x_feat = self.feat_branch(feat.flatten(0, 1)).unflatten(0, [B, N])
+            x_config = self.config_branch(configs.flatten(0, 1).to(feat.device)).unflatten(0, [B, N])
             # CHECK: batch_first=True for transformer
             token_location = (torch.arange(N).repeat([B, 1]) == step[:, None] + 1)
-            query = x_feat + x_config
-            query[token_location] = self.state_token.expand(B, -1).to(feat.device)
-            query += self.positional_embedding[None, :N].to(feat.device)
-            query_padding_mask = (torch.arange(N).repeat([B, 1]) > step[:, None] + 1).to(feat.device)
-            # key = x_feat + x_config + self.positional_embedding[None, :N].to(feat.device)
-            # key_padding_mask = (torch.arange(N).repeat([B, 1]) > step[:, None]).to(feat.device)
-            # nn.MultiheadAttention requires *binary* mask=True for locations that we do not want to attend
-            # x = self.transformer(query, key,
-            #                      tgt_key_padding_mask=query_padding_mask,
-            #                      memory_key_padding_mask=key_padding_mask)
-            x = self.transformer(query, src_key_padding_mask=query_padding_mask)
+            x = x_feat + x_config
+            x[token_location] = self.state_token.to(feat.device)
+            x += self.positional_embedding[None, :N].to(feat.device)
+            mask = (torch.arange(N).repeat([B, 1]) > step[:, None] + 1).to(feat.device)
+            x = self.transformer(x, src_key_padding_mask=mask)
             # Classifier "token" as used by standard language architectures
             x = x[token_location]
         else:
             # conv + fc only
-            x_feat = self.feat_branch(feat.max(dim=1)[0])[:, :, 0, 0]
+            x_feat = self.feat_branch(feat.max(dim=1)[0])
             x_config = self.config_branch(configs.to(feat.device).flatten(1, 2))
             x = x_feat + x_config
 
@@ -101,3 +88,24 @@ class CamControl(nn.Module):
         if action is None:
             action = probs.sample()
         return action, probs.log_prob(action).sum(1), probs.entropy().sum(1), self.critic(x)
+
+
+if __name__ == '__main__':
+    from torchvision.models import vit_b_16
+    from torchvision.models import vgg16, alexnet
+
+
+    class Object(object):
+        pass
+
+
+    B, N, C, H, W = 8, 4, 128, 200, 200
+    dataset = Object()
+    dataset.config_dim = 7
+    dataset.action_dim = 2
+    dataset.num_cam = 4
+
+    model = CamControl(dataset, C, )
+
+    state = (torch.randn([B, N, C, H, W]), torch.randn([B, N, dataset.config_dim]), torch.randint(0, N - 1, [B]))
+    model.get_action_and_value(state)
