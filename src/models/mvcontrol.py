fe51b11237a68f3ca8419be5cf3b3882ef813297
Yunzhong Hou <hou_yz@outlook.com>
2023-09-19 11:35:23 +1000

    move function to tensor_utils.py

--- src/models/mvcontrol.py
+++ src/models/mvcontrol.py
@@ -23,62 +23,6 @@ def create_pos_embedding(L, hidden_dim=128, temperature=10000, ):
     return pe
 
 
-def tanh_prime(x, thres=20):
-    # tanh'(x) = torch.log(4 / (torch.exp(x) + torch.exp(-x)) ** 2))
-    output = np.log(4) - 2 * torch.log(torch.exp(x) + torch.exp(-x))
-    # For numerical stability the implementation reverts to the linear function when |x| > thres
-    output[x > thres] = np.log(4) - 2 * x[x > thres]
-    output[x < -thres] = np.log(4) + 2 * x[x < -thres]
-    return output
-
-
-def dist_action(act1, act2, action_names):
-    dist_ = None
-    if 'x' in action_names and 'y' in action_names:
-        idx = [action_names.index('x'), action_names.index('y')]
-        xy1, xy2 = act1[..., idx], act2[..., idx]
-        dist_loc_ = dist_location(xy1, xy2)
-        dist_ = dist_ + dist_loc_ if dist_ is not None else dist_loc_
-    if 'yaw' in action_names:
-        idx = action_names.index('yaw')
-        yaw1, yaw2 = act1[..., idx], act2[..., idx]
-        dist_rot_ = dist_rotation(yaw1, yaw2)
-        dist_ = dist_ + dist_rot_ if dist_ is not None else dist_rot_
-    return dist_
-
-
-def dist_location(xy1, xy2):
-    return ((xy1 - xy2) ** 2).sum(-1) ** 0.5
-
-
-def dist_rotation(yaw1, yaw2):
-    angle = torch.abs(yaw1 * 180 - yaw2 * 180)
-    return torch.min(angle, 360 - angle) / 180
-
-
-def expectation(probs, x_range, func, n_points=1000, device='cpu'):
-    if isinstance(x_range[0], float):
-        B, C = 1, 1
-        x_range = [torch.ones(B, C) * (x_range[0]), torch.ones(B, C) * (x_range[0])]
-    elif len(x_range[0].shape) == 0:
-        B, C = 1, 1
-        x_range = [x_range[0].reshape([B, C]), x_range[0].reshape([B, C])]
-    elif len(x_range[0].shape) == 1:
-        B, C = 1, x_range.shape[0]
-        x_range = [x_range[0].reshape([B, C]), x_range[0].reshape([B, C])]
-    elif len(x_range[0].shape) == 2:
-        B, C = x_range[0].shape
-        pass
-    else:
-        raise Exception
-    x = torch.empty([B, C, n_points], device=device)
-    for b in range(B):
-        for c in range(C):
-            x[b, c] = torch.linspace(float(x_range[0][b, c]), float(x_range[1][b, c]), n_points, device=device)
-    log_probs = torch.stack([probs.log_prob(x[:, :, i]) for i in range(n_points)], dim=2)
-    return torch.trapz(torch.exp(log_probs) * func(x), x)
-
-
 class CamControl(nn.Module):
     def __init__(self, dataset, hidden_dim, arch='transformer', actstd_init=1.0):
         super().__init__()
@@ -156,6 +100,7 @@ if __name__ == '__main__':
     import tqdm
     from torchvision.models import vit_b_16
     from torchvision.models import vgg16, alexnet
+    from src.utils.tensor_utils import dist_action, dist_location, dist_rotation
 
 
     class Object(object):
